13696ba8585927b089c581ec1ee34383f643ed83
mfritze <mfritze@ualberta.ca>
2015-09-06 13:13:23 -0600

    Added brick lives

--- brickBreaker.js
+++ brickBreaker.js
@@ -21,41 +21,27 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var brickList = [];
+var brickColors = ["#FD5961", "#FA9D82", "#EBE395", "#78AD88", "#08576B"];
+var total = brickRowCount * brickColumnCount;
 
+var intervalID = setInterval(draw, 8);
 initializeBricks();
-setInterval(draw, 8);
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function draw() {
     context.clearRect(0,0, canvas.width, canvas.height);
-
-    if ( ballY + dy == ballRadius){
-        dy = -dy;
-    }
-    else if (ballY + dy == canvas.height - (ballRadius + paddleHeight) &&
-        ballX >= paddleX && ballX <= paddleX + paddleWidth ) {
-        dy = -dy;
-    }else if (ballY + dy == canvas.height - ballRadius){
-        alert("Game over bruh");
-        document.location.reload();
-    }
-
-    if ( ballX + dx == canvas.width - ballRadius || ballX + dx == ballRadius){
-        dx = -dx;
-    }
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
-    }
-    else if (leftPressed && paddleX > 0){
-        paddleX -= 7;
-    }
+    move();
+    wallCollisionDetection();
     brickCollisionDetection();
+    paddleCollision();
+    checkVictory();
+    ballX += dx;
+    ballY += dy;
     drawPaddle();
     drawBall();
     drawBricks();
-    ballX += dx;
-    ballY += dy;
 }
 
 function drawBall(){
@@ -80,32 +66,22 @@ function drawBricks() {
         var brick = brickList[i];
         context.beginPath();
         context.rect(brick.x, brick.y, brickWidth, brickHeight);
-        context.fillStyle = "#0095DD";
+        context.fillStyle = brickColors[brick.lives];
         context.fill();
         context.closePath();
     }
 }
 
-function brickCollisionDetection() { // TODO this should be more sophisticated by checking with radius of the ball
-    //for (var c = 0; c < brickColumnCount; c++) {
-    //    for (var r = 0; r < brickRowCount; r++) {
-    //        var brick = bricks[c][r];
-    //        if (brick.status == 1) {
-    //            if (ballX > brick.x && ballX < brick.x + brickWidth && ballY > brick.y && ballY < brick.y+brickHeight) {
-    //                dy = -dy;
-    //                brick.status = 0;
-    //            }
-    //        }
-    //    }
-    //}
+
+function brickCollisionDetection() {
     for (var i = 0; i < brickList.length; i++){
         var brick = brickList[i];
         var fX = ballX + dx; // future x
         var fY = ballY + dy; // future y
-        var topEdge = brick.y;
-        var bottomEdge = brick.y + brickHeight;
-        var leftEdge = brick.x;
-        var rightEdge = brick.x + brickWidth;
+        var topEdge = brick.y - ballRadius;
+        var bottomEdge = brick.y + brickHeight + ballRadius;
+        var leftEdge = brick.x - ballRadius;
+        var rightEdge = brick.x + brickWidth + ballRadius;
 
         // Any collision
         if (( leftEdge <= fX && fX <= rightEdge) &&
@@ -120,6 +96,14 @@ function brickCollisionDetection() { // TODO this should be more sophisticated b
                 dx = -dx;
             }
 
+            if   ((ballX < leftEdge) &&
+                (((ballY > bottomEdge) && ((leftEdge - fX) <= (bottomEdge - fY)) ) ||
+                ((ballY < topEdge)  && ((leftEdge - fX) <= (topEdge - fY)) ) ||
+                (ballY <= bottomEdge && ballY >= topEdge ) )
+            ){
+                dx = -dx;
+            }
+
             // Bottom
             if  ((ballY > bottomEdge)&&
                (((ballX > rightEdge) && ((bottomEdge - fY) <= (rightEdge - fX)) ) ||
@@ -129,9 +113,54 @@ function brickCollisionDetection() { // TODO this should be more sophisticated b
                 dy = -dy;
             }
 
-            brickList.splice(i, 1);
+            if  ((ballY < topEdge)&&
+                (((ballX > rightEdge) && ((topEdge - fY) <= (rightEdge - fX)) ) ||
+                ((ballX < leftEdge ) && ((topEdge - fY) <= (leftEdge  - fX)) ) ||
+                (ballX <= rightEdge && ballX >= leftEdge) )
+            ){
+                dy = -dy;
+            }
+            brickList[i].lives -= 1;
+            if(brickList[i].lives == -1){
+                brickList.splice(i,1);
+            }
         }
 
+    }
+}
+
+function wallCollisionDetection() {
+    if ( ballY + dy == ballRadius){
+        dy = -dy;
+    }else if (ballY + dy == canvas.height - ballRadius){
+        alert("Game over bruh");
+        document.location.reload();
+    }
+    if ( ballX + dx == canvas.width - ballRadius || ballX + dx == ballRadius){
+        dx = -dx;
+    }
+}
+
+function paddleCollision(){
+    // coming from left  -> left paddle: slow down
+    //                   -> right paddle : speed up
+    // coming from right -> right paddle: slow down
+    //                   -> left paddle : speed up
+    // Middle third always remains constant.
+
+    if (ballY + dy == canvas.height - (ballRadius + paddleHeight) &&
+        ballX >= paddleX && ballX <= paddleX + paddleWidth ) {
+        dy = -dy;
+    }
+
+}
+
+function move(){
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0){
+        paddleX -= 7;
     }
 }
 
@@ -157,7 +186,17 @@ function initializeBricks() {
     for(i = 0; i < brickColumnCount * brickRowCount; i++) {
         var brickX = (i % brickColumnCount) * (brickWidth + brickPadding) + brickOffsetLeft;
         var brickY = (i % brickRowCount) * (brickHeight + brickPadding) + brickOffsetTop;
-        var bonus = true;
-        brickList.push( {x: brickX, y: brickY, bonus: bonus});
+        var lives = (Math.pow(i, 2) % 3) + (Math.pow((i+7), 2) %4);
+        brickList.push( {x: brickX, y: brickY, lives: lives});
+    }
+}
+
+function checkVictory() {
+    if (brickList.length == 0){
+        clearInterval(intervalID);
+        context.clearRect(0,0, canvas.width, canvas.height);
+        context.font = "40px Open Sans";
+        context.fillStyle = "#0095DD";
+        context.fillText("You Win! Play again?", canvas.width/2 - 120, canvas.height*(3/5));
     }
 }
